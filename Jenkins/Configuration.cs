2316c304add79f86afc4860c3e12e7622b88d3b8
Federico Azzato <fedeazzato@gmail.com>
2026-03-11 11:54:58 -0300

    Update build and doc (#3)
    * Switch reference to GX_SERVER_DIR
    * Remove unused Result classes
    * Code clean-up
    * Update documentation

--- Jenkins/Configuration.cs
+++ Jenkins/Configuration.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace GeneXus.Server.ExternalTool.Jenkins
@@ -11,6 +7,5 @@ namespace GeneXus.Server.ExternalTool.Jenkins
     {
         [JsonProperty("instance")]
         public List<JenkinsServer> JenkinsInstance { get; set; }
-
     }
 }
